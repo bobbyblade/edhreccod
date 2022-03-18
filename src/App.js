ef6b11360cd6086c8a5f8b7d305fc789296b3ddf
bobbyblade <bobbyblade@gmail.com>
2022-03-18 10:24:08 -0500

    Updated initial website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [data,setData]=useState([]);
+  let url = 'https://edhreccod-dev-serverlessdeploymentbucket-xyf9ylmxxem6.s3.amazonaws.com/serverless/edhreccod/json/ercod.json';
+  let local_url = 'ercod.json';
+  const getData=()=>{
+    fetch(url
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(myJson) {
+        setData(myJson)
+      });
+  }
+  useEffect(()=>{
+    getData()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {
+       data && data.length>0 && data.map((item)=><p><auto-card key={item.date}>{item.name}</auto-card> - {new Date(item.date).toLocaleDateString('en-us')}</p>)
+     }
     </div>
   );
 }

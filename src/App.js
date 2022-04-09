e118f244d50281639baff6ff2c746e6789707774
Bobby Blade <bobbyblade@gmail.com>
2022-04-09 00:46:04 -0500

    Added date picker to filter by specific date.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,82 @@
-import React,{useState,useEffect} from 'react';
+import React,{ Component } from 'react';
 import './App.css';
 import Footer from './footer'
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function Card(props) {
   return <p key={props.date}>{new Date(props.date).toLocaleDateString('en-us')}<br /><a href={"https://scryfall.com/card/".concat(props.scryfall_uri)}><img alt={props.name} src={props.img} /></a><br />{props.name}</p>
 }
-function App() {
-  const [data,setData]=useState([]);
-  let url = 'https://edhreccod-dev-serverlessdeploymentbucket-xyf9ylmxxem6.s3.amazonaws.com/serverless/edhreccod/json/ercod.json';
-  let local_url = 'ercod.json';
-  const getData=()=>{
-    fetch(url
-    ,{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    }
-    )
-      .then(function(response){
-        return response.json();
-      })
-      .then(function(myJson) {
-        setData(myJson)
-      });
+class App extends Component {
+
+  constructor (props) {
+    super(props)
+    //let url = 'https://edhreccod-dev-serverlessdeploymentbucket-xyf9ylmxxem6.s3.amazonaws.com/serverless/edhreccod/json/ercod.json';
+    let local_url = 'ercod.json';
+    this.url = local_url;
+    this.state = {
+      startDate: new Date(),
+      data: {},
+      datePicked: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.data = {};
   }
-  useEffect(()=>{
-    getData()
-  },[])
+  handleChange(date) {
+    this.setState({
+      startDate: date,
+      datePicked: true
+    })
+  }
+  onFormSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.startDate)
+    this.setState({datePicked: false});
+  }
+  componentDidMount(){
+    this.getData();
+  }
+  async getData(){
+    const res = await fetch(this.url
+      ,{
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+      }
+      );
+    const data = await res.json();
+    console.log(data);
+    return this.setState({data:data});
+ }
+
+  render() {
+    var data,
+    data = this.state.data,
+    searchDate = this.state.startDate,
+    datePicked = this.state.datePicked;
+    if(datePicked) {
+      data = data.filter( l=> {
+        console.log(searchDate);
+        console.log(new Date(l.date).toLocaleDateString('en-us'))
+        return new Date(l.date).toLocaleDateString('en-us').match( new Date(searchDate).toLocaleDateString('en-us') );
+      });
+    }
   return (
     <div className="App">
+      <form onSubmit={ this.onFormSubmit }>
+        <div className="form-group">
+          <DatePicker
+              selected={ this.state.startDate }
+              onChange={ this.handleChange }
+              name="startDate"
+              dateFormat="MM/dd/yyyy"
+          />
+          <button className="btn btn-primary">Reset</button>
+        </div>
+      </form>
       <div className="card-container">
      {
        data && data.length>0 && data.map((item)=><Card key={item.date} date={item.date} scryfall_uri={item.scryfall_uri} name={item.name} img={item.img} />)
@@ -37,6 +85,7 @@ function App() {
     <Footer/>
     </div>
   );
+  }
 }
 
 export default App;
